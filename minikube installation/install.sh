1c84ebce2d645caa161a5349e07330e6226c04ab
KelvinLee <44488127+kelvinlee97@users.noreply.github.com>
2025-01-31 17:34:04 +0800

    Update install.sh

--- minikube installation/install.sh
+++ minikube installation/install.sh
@@ -1,32 +1,51 @@
 #!/bin/bash
 
-# system up to date
-sudo yum update -y
+yum_update() {	
+	sudo yum update -y > /dev/null 2>&1 &
+	echo "yum update done~"
+}
+install_tools() {
+	sudo yum install -y curl wget git socat conntrack > /dev/null 2>&1 &
+	echo "install_tools done~"
+}
+install_docker () {
+	sudo amazon-linux-extras enable docker > /dev/null 2>&1 &
+	sudo yum install -y docker > /dev/null 2>&1 &
+	sudo systemctl start docker > /dev/null 2>&1 &
+	sudo systemctl enable docker > /dev/null 2>&1 &
+	echo "install_docker and start done~"
+}
+docker_user() {
+	sudo usermod -aG docker $USER > /dev/null 2>&1 &
+  echo "docker_user done~"
+}	
+minikube() {
+	curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64 > /dev/null 2>&1 &
+	sudo install minikube-linux-amd64 /usr/bin/minikube > /dev/null 2>&1 &
+	echo "minikube done~"
+}	
+kubectl() {
+	curl -LO "https://dl.k8s.io/release/v1.32.0/bin/linux/amd64/kubectl" > /dev/null 2>&1 &
+	sudo install kubectl /usr/bin/kubectl > /dev/null 2>&1 &
+	echo "kubectl done~"
+}
+start_minikube(){
+	minikube start --driver=docker --force > /dev/null 2>&1 &
+	echo "start minikube done~"
+}
 
-# install some common tools
-sudo yum install -y curl wget git socat conntrack
+chown_minikube(){
+	sudo chown -R $USER:$USER $HOME/.kube $HOME/.minikube > /dev/null 2>&1 &
+	echo "chown minikube done~"
+}
 
-# install docker
-sudo amazon-linux-extras enable docker
-sudo yum install -y docker
-sudo systemctl start docker
-sudo systemctl enable docker
-
-# configure to docker user
-sudo usermod -aG docker $USER
-
-# download && install minikube
-curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
-sudo install minikube-linux-amd64 /usr/bin/minikube
-
-# download && install kubectl
-curl -LO "https://dl.k8s.io/release/v1.32.0/bin/linux/amd64/kubectl"
-sudo install kubectl /usr/bin/kubectl
-
-# start minikube
-minikube start --driver=docker --force
-
-# Configure Permissions for kubectl
-sudo chown -R $USER:$USER $HOME/.kube $HOME/.minikube
+yum_update
+install_tools
+install_docker
+docker_user
+minikube
+kubectl
+start_minikube
+chown_minikube
 
 exit
