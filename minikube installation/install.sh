25f20a8ab344785886636775180284294df4ab32
KelvinLee <44488127+kelvinlee97@users.noreply.github.com>
2025-02-12 10:28:31 +0800

    Update install.sh

--- minikube installation/install.sh
+++ minikube installation/install.sh
@@ -1,41 +1,41 @@
 #!/bin/bash
 
 yum_update() {	
-	sudo yum update -y > /dev/null 2>&1 &
+	sudo yum update -y > /dev/null 2>&1
 	echo "yum update done~"
 }
 install_tools() {
-	sudo yum install -y curl wget git socat conntrack > /dev/null 2>&1 &
+	sudo yum install -y curl wget git socat conntrack > /dev/null 2>&1
 	echo "install_tools done~"
 }
 install_docker () {
-	sudo amazon-linux-extras enable docker > /dev/null 2>&1 &
-	sudo yum install -y docker > /dev/null 2>&1 &
-	sudo systemctl start docker > /dev/null 2>&1 &
-	sudo systemctl enable docker > /dev/null 2>&1 &
+	sudo amazon-linux-extras enable docker > /dev/null 2>&1
+	sudo yum install -y docker > /dev/null 2>&1
+	sudo systemctl start docker > /dev/null 2>&1
+	sudo systemctl enable docker > /dev/null 2>&1
 	echo "install_docker and start done~"
 }
 docker_user() {
-	sudo usermod -aG docker $USER > /dev/null 2>&1 &
+	sudo usermod -aG docker $USER > /dev/null 2>&1
   echo "docker_user done~"
 }	
 minikube() {
-	curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64 > /dev/null 2>&1 &
-	sudo install minikube-linux-amd64 /usr/bin/minikube > /dev/null 2>&1 &
+	curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64 > /dev/null 2>&1
+	sudo install minikube-linux-amd64 /usr/bin/minikube > /dev/null 2>&1
 	echo "minikube done~"
 }	
 kubectl() {
-	curl -LO "https://dl.k8s.io/release/v1.32.0/bin/linux/amd64/kubectl" > /dev/null 2>&1 &
-	sudo install kubectl /usr/bin/kubectl > /dev/null 2>&1 &
+	curl -LO "https://dl.k8s.io/release/v1.32.0/bin/linux/amd64/kubectl" > /dev/null 2>&1
+	sudo install kubectl /usr/bin/kubectl > /dev/null 2>&1
 	echo "kubectl done~"
 }
 start_minikube(){
-	minikube start --driver=docker --force > /dev/null 2>&1 &
+	minikube start --driver=docker --force > /dev/null 2>&1
 	echo "start minikube done~"
 }
 
 chown_minikube(){
-	sudo chown -R $USER:$USER $HOME/.kube $HOME/.minikube > /dev/null 2>&1 &
+	sudo chown -R $USER:$USER $HOME/.kube $HOME/.minikube > /dev/null 2>&1
 	echo "chown minikube done~"
 }
 
