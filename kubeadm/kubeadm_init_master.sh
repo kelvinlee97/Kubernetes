8107092916c01cf84a8c705ad035a85d537a0318
KelvinLee <44488127+kelvinlee97@users.noreply.github.com>
2025-02-24 16:23:00 +0800

    Update kubeadm_init_master.sh

--- kubeadm/kubeadm_init_master.sh
+++ kubeadm/kubeadm_init_master.sh
@@ -2,17 +2,16 @@
 
 # this actions will be suit on Debian OS(Ubuntu)
 
-MyInternalIp="10.0.1.223"
-MyPodCIDR="10.0.2.0/24"
-K8sVersion="v1.32"
+MyInternalIp="10.0.1.222"
+MyPodCIDR="10.0.1.0/24"
 
 # regular update and install tools need
 sudo apt-get update -y
-sudo apt-get install -y apt-transport-https ca-certificates curl gpg
+sudo apt-get install -y apt-transport-https ca-certificates curl gpg net-tools
 
 # install kubelet kubeadm kubectl with v1.32 version
-curl -fsSL https://pkgs.k8s.io/core:/stable:/$K8sVersion/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
-echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/$K8sVersion/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
+curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.31/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.31/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
 sudo apt-get update -y
 sudo apt-get install -y kubelet kubeadm kubectl
 sudo apt-mark hold kubelet kubeadm kubectl
@@ -26,13 +25,12 @@ sudo systemctl restart containerd.service
 
 # Enable IP Forwarding and iptable bridge
 sudo tee /etc/sysctl.d/kubernetes.conf <<EOF
-net.ipv4.ip_forward=1
-net.bridge.bridge-nf-call-iptables=1
+net.bridge.bridge-nf-call-iptables = 1
+net.ipv4.ip_forward = 1
 EOF
 
 sudo sysctl --system
-sysctl net.ipv4.ip_forward
-sysctl net.bridge.bridge-nf-call-iptables
+sudo modprobe br_netfilter
 
 # initialize kubeadm master 
 sudo kubeadm init --apiserver-advertise-address $MyInternalIp --pod-network-cidr "$MyPodCIDR" --upload-certs > /tmp/kubeadm_output.txt
@@ -44,8 +42,8 @@ sudo chown $(id -u):$(id -g) $HOME/.kube/config
 # install network plugin(flannel)
 cd /etc/kubernetes/manifests/
 sudo wget https://github.com/flannel-io/flannel/releases/latest/download/kube-flannel.yml
-sudo kubectl apply -f /etc/kubernetes/manifests/kube-flannel.yml --validate=false
+#sudo kubectl apply -f /etc/kubernetes/manifests/kube-flannel.yml --validate=false
 
 # kubectl auto-completion
-echo 'source <(sudo kubectl completion bash)' >> ~/.bashrc
-source ~/.bashrc
+echo 'source <(kubectl completion bash)' >> ~/.bashrc
+/bin/bash -c 'source ~/.bashrc'
