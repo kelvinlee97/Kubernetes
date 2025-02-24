1559c1d3db77f844aca3cccdcca0ecb90f90bfaf
KelvinLee <44488127+kelvinlee97@users.noreply.github.com>
2025-02-24 15:12:33 +0800

    Update kubeadm_init_worker.sh

--- kubeadm/kubeadm_init_worker.sh
+++ kubeadm/kubeadm_init_worker.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 
 # this actions will be suit on Debian OS(Ubuntu)
-K8sVersion=1.32
 
 # regular update and install tools need
 sudo apt-get update -y
 sudo apt-get install -y apt-transport-https ca-certificates curl gpg
 
 # install kubelet kubeadm kubectl with v1.32 version
-curl -fsSL https://pkgs.k8s.io/core:/stable:/v$K8sVersion/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
-echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v$K8sVersion/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
+curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.30/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.30/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
 sudo apt-get update -y
 sudo apt-get install -y kubelet kubeadm kubectl
 sudo apt-mark hold kubelet kubeadm kubectl
@@ -23,14 +22,13 @@ sudo systemctl restart containerd.service
 
 # Enable IP Forwarding and iptable bridge
 sudo tee /etc/sysctl.d/kubernetes.conf <<EOF
-net.ipv4.ip_forward=1
-net.bridge.bridge-nf-call-iptables=1
+net.bridge.bridge-nf-call-iptables = 1
+net.ipv4.ip_forward = 1
 EOF
 
 sudo sysctl --system
-sysctl net.ipv4.ip_forward
-sysctl net.bridge.bridge-nf-call-iptables
+sudo modprobe br_netfilter
 
 # kubectl auto-completion
-#echo 'source <(sudo kubectl completion bash)' >> ~/.bashrc
-#source ~/.bashrc
+echo 'source <(kubectl completion bash)' >> ~/.bashrc
+/bin/bash -c 'source ~/.bashrc'
