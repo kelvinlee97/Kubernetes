e8f31e11e7b38be4130d6cfaf250ff6846e3c303
KelvinLee <44488127+kelvinlee97@users.noreply.github.com>
2025-02-24 16:22:40 +0800

    Update kubeadm_init_worker.sh

--- kubeadm/kubeadm_init_worker.sh
+++ kubeadm/kubeadm_init_worker.sh
@@ -7,8 +7,8 @@ sudo apt-get update -y
 sudo apt-get install -y apt-transport-https ca-certificates curl gpg
 
 # install kubelet kubeadm kubectl with v1.32 version
-curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.30/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
-echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.30/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
+curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.32/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.32/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
 sudo apt-get update -y
 sudo apt-get install -y kubelet kubeadm kubectl
 sudo apt-mark hold kubelet kubeadm kubectl
@@ -21,13 +21,17 @@ containerd config default | sed 's/SystemdCgroup = false/SystemdCgroup = true/' 
 sudo systemctl restart containerd.service
 
 # Enable IP Forwarding and iptable bridge
-sudo tee /etc/sysctl.d/kubernetes.conf <<EOF
-net.bridge.bridge-nf-call-iptables = 1
-net.ipv4.ip_forward = 1
+sudo swapoff -a
+sudo modprobe overlay
+sudo modprobe br_netfilter
+
+cat <<EOF | sudo tee /etc/sysctl.d/k8s.conf
+net.bridge.bridge-nf-call-iptables  = 1
+net.bridge.bridge-nf-call-ip6tables = 1
+net.ipv4.ip_forward                 = 1
 EOF
 
 sudo sysctl --system
-sudo modprobe br_netfilter
 
 # kubectl auto-completion
 echo 'source <(kubectl completion bash)' >> ~/.bashrc
