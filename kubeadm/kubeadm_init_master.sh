3c9bc158b1c422bc19bb1871c7c63ae9bdc0ab29
KelvinLee <44488127+kelvinlee97@users.noreply.github.com>
2025-02-25 10:33:12 +0800

    Update kubeadm_init_master.sh

--- kubeadm/kubeadm_init_master.sh
+++ kubeadm/kubeadm_init_master.sh
@@ -1,49 +1,38 @@
 #!/bin/bash
 
-# this actions will be suit on Debian OS(Ubuntu)
-
-MyInternalIp="10.0.1.222"
-MyPodCIDR="10.0.1.0/24"
-
-# regular update and install tools need
-sudo apt-get update -y
-sudo apt-get install -y apt-transport-https ca-certificates curl gpg net-tools
-
-# install kubelet kubeadm kubectl with v1.32 version
-curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.31/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
-echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.31/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
-sudo apt-get update -y
-sudo apt-get install -y kubelet kubeadm kubectl
-sudo apt-mark hold kubelet kubeadm kubectl
-sudo systemctl enable --now kubelet
-
-# install containerd
-sudo apt install containerd -y
-sudo mkdir -p /etc/containerd/
-containerd config default | sed 's/SystemdCgroup = false/SystemdCgroup = true/' | sudo tee /etc/containerd/config.toml
-sudo systemctl restart containerd.service
-
-# Enable IP Forwarding and iptable bridge
-sudo tee /etc/sysctl.d/kubernetes.conf <<EOF
+sudo apt update && sudo apt upgrade -y
+sudo swapoff -a
+sudo sed -i '/ swap / s/^/#/' /etc/fstab
+sudo modprobe overlay
+sudo modprobe br_netfilter
+echo -e "overlay\nbr_netfilter" | sudo tee /etc/modules-load.d/k8s.conf
+cat <<EOF | sudo tee /etc/sysctl.d/kubernetes.conf
+net.bridge.bridge-nf-call-ip6tables = 1
 net.bridge.bridge-nf-call-iptables = 1
 net.ipv4.ip_forward = 1
 EOF
-
 sudo sysctl --system
-sudo modprobe br_netfilter
-
-# initialize kubeadm master 
-sudo kubeadm init --apiserver-advertise-address $MyInternalIp --pod-network-cidr "$MyPodCIDR" --upload-certs > /tmp/kubeadm_output.txt
-
+sudo apt update
+sudo apt install -y containerd
+sudo mkdir -p /etc/containerd
+containerd config default | sudo tee /etc/containerd/config.toml
+sudo sed -i 's/SystemdCgroup = false/SystemdCgroup = true/' /etc/containerd/config.toml
+sudo systemctl restart containerd
+sudo systemctl enable containerd
+sudo apt update
+sudo apt install -y apt-transport-https curl
+curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.31/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+echo "deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.31/deb/ /" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+sudo apt update
+sudo apt install -y kubelet kubeadm kubectl
+sudo apt-mark hold kubelet kubeadm kubectl
+# only master node apply below these
+sudo kubeadm init --pod-network-cidr=10.244.0.0/16
 mkdir -p $HOME/.kube
 sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
 sudo chown $(id -u):$(id -g) $HOME/.kube/config
-
-# install network plugin(flannel)
 cd /etc/kubernetes/manifests/
-sudo wget https://github.com/flannel-io/flannel/releases/latest/download/kube-flannel.yml
-#sudo kubectl apply -f /etc/kubernetes/manifests/kube-flannel.yml --validate=false
-
-# kubectl auto-completion
-echo 'source <(kubectl completion bash)' >> ~/.bashrc
-/bin/bash -c 'source ~/.bashrc'
+sudo wget https://raw.githubusercontent.com/flannel-io/flannel/master/Documentation/kube-flannel.yml
+kubectl apply -f kube-flannel.yml
+source <(kubectl completion bash)
+# kubeadm token create --print-join-command
