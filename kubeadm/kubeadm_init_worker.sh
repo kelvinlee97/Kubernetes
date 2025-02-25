127afc25c9ca85a10f191b836fd18b90f5751831
KelvinLee <44488127+kelvinlee97@users.noreply.github.com>
2025-02-25 10:26:31 +0800

    Update kubeadm_init_worker.sh

--- kubeadm/kubeadm_init_worker.sh
+++ kubeadm/kubeadm_init_worker.sh
@@ -1,38 +1,28 @@
 #!/bin/bash
 
-# this actions will be suit on Debian OS(Ubuntu)
-
-# regular update and install tools need
-sudo apt-get update -y
-sudo apt-get install -y apt-transport-https ca-certificates curl gpg
-
-# install kubelet kubeadm kubectl with v1.32 version
-curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.32/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
-echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.32/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
-sudo apt-get update -y
-sudo apt-get install -y kubelet kubeadm kubectl
-sudo apt-mark hold kubelet kubeadm kubectl
-sudo systemctl enable --now kubelet
-
-# install containerd
-sudo apt install containerd -y
-sudo mkdir -p /etc/containerd/
-containerd config default | sed 's/SystemdCgroup = false/SystemdCgroup = true/' | sudo tee /etc/containerd/config.toml
-sudo systemctl restart containerd.service
-
-# Enable IP Forwarding and iptable bridge
+sudo apt update && sudo apt upgrade -y
 sudo swapoff -a
+sudo sed -i '/ swap / s/^/#/' /etc/fstab
 sudo modprobe overlay
 sudo modprobe br_netfilter
-
-cat <<EOF | sudo tee /etc/sysctl.d/k8s.conf
-net.bridge.bridge-nf-call-iptables  = 1
+echo -e "overlay\nbr_netfilter" | sudo tee /etc/modules-load.d/k8s.conf
+cat <<EOF | sudo tee /etc/sysctl.d/kubernetes.conf
 net.bridge.bridge-nf-call-ip6tables = 1
-net.ipv4.ip_forward                 = 1
+net.bridge.bridge-nf-call-iptables = 1
+net.ipv4.ip_forward = 1
 EOF
-
 sudo sysctl --system
-
-# kubectl auto-completion
-echo 'source <(kubectl completion bash)' >> ~/.bashrc
-/bin/bash -c 'source ~/.bashrc'
+sudo apt update
+sudo apt install -y containerd
+sudo mkdir -p /etc/containerd
+containerd config default | sudo tee /etc/containerd/config.toml
+sudo sed -i 's/SystemdCgroup = false/SystemdCgroup = true/' /etc/containerd/config.toml
+sudo systemctl restart containerd
+sudo systemctl enable containerd
+sudo apt update
+sudo apt install -y apt-transport-https curl
+curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.31/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+echo "deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.31/deb/ /" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+sudo apt update
+sudo apt install -y kubelet kubeadm kubectl
+sudo apt-mark hold kubelet kubeadm kubectl
