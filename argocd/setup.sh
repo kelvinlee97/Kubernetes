10c3d88e79a1d68a9c0a7c344a0a8c440a571f27
KelvinLee <44488127+kelvinlee97@users.noreply.github.com>
2025-04-03 14:19:27 +0800

    Update setup.sh

--- argocd/setup.sh
+++ argocd/setup.sh
@@ -3,3 +3,10 @@
 kubectl create namespace argocd
 kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
 kubectl patch svc argocd-server -n argocd -p '{"spec": {"type": "NodePort"}}'
+
+# argocd cli
+sudo curl -sSL -o argocd https://github.com/argoproj/argo-cd/releases/latest/download/argocd-linux-amd64
+sudo chmod +x argocd
+sudo mv argocd /usr/local/bin/
+# check version
+#argocd version --client
